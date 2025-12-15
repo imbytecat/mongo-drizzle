b18686413a10e996cd82297458846da0bda849da
imbytecat <imbytecat@gmail.com>
2025-12-15 13:16:05 +0800

    test: 更新测试用例使用 Date 对象并添加注释
    - 修改测试用例以使用 Date 对象替代日期字符串并添加待解决的 JSON 序列化问题注释。

--- test/index.test.ts
+++ test/index.test.ts
@@ -29,7 +29,8 @@ describe('mongoQueryBuilder', () => {
     })
 
     it('应该正确过滤 timestamp 字段的 $lte 操作符', async () => {
-      const targetDate = '2025-01-01T00:00:00.000Z'
+      // const targetDate = '2025-01-01T00:00:00.000Z'
+      const targetDate = new Date('2025-01-01T00:00:00.000Z') // TODO: 解决 JSON 不能传输 Date 的问题
       const qb = mongoQueryBuilder(schema.tests, {
         find: {
           timestamp: {
