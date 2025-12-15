785d0ab236e80f64ffc33e537299d48e30f88d91
imbytecat <imbytecat@gmail.com>
2025-12-15 12:18:45 +0800

    chore: 添加PostgreSQL驱动并调整主键生成函数
    - 添加PostgreSQL数据库驱动依赖。
    - 添加Postgres作为开发依赖以支持数据库连接。
    - 将主键ID的默认生成函数从uuid_generate_v7()更改为uuidv7()。

--- test/db/schema/tests.ts
+++ test/db/schema/tests.ts
@@ -2,7 +2,7 @@ import { sql } from 'drizzle-orm'
 import { numeric, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
 
 export const tests = pgTable('tests', {
-  id: uuid('id').primaryKey().default(sql`uuid_generate_v7()`),
+  id: uuid('id').primaryKey().default(sql`uuidv7()`),
   text: text(),
   numeric: numeric(),
   timestamp: timestamp({ precision: 6, withTimezone: true }),
